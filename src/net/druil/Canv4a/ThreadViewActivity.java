0f7e8c491d62e5249807b7ba108ef8a97c671560
Paul Olliver <paul.ollivier@druil.net>
2012-03-28 19:16:01 +0200

    Added The Thread view Activity

---
+++ src/net/druil/Canv4a/ThreadViewActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.druil.Canv4a;
+
+import org.apache.http.client.HttpClient;
+import org.json.JSONObject;
+
+import android.app.Activity;
+
+/**
+ * @author Paul
+ *
+ */
+public class ThreadViewActivity extends Activity {
+
+	HttpClient clt;
+	JSONObject res;
+
+}
