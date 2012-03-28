2412044ceb8447da3211f4319ff469ee2cfd9923
Paul Olliver <paul.ollivier@druil.net>
2012-03-28 20:56:47 +0200

    Data-fetching and parsing works.

--- src/net/druil/Canv4a/ThreadViewActivity.java
+++ src/net/druil/Canv4a/ThreadViewActivity.java
@@ -6,12 +6,12 @@ package net.druil.Canv4a;
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -37,6 +37,22 @@ public class ThreadViewActivity extends Activity {
 			HttpResponse getResponse = clt.execute(new HttpGet(url));
 			Log.d("API",EntityUtils.toString(getResponse.getEntity()));
 		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			res = new JSONObject(EntityUtils.toString(clt.execute(new HttpGet(url)).getEntity()));
+			Log.d("API",res.getString("category"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
