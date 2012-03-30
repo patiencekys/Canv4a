6e892f0991e0fc0aadf062f850586ca16a25c426
Paul Olliver <paul.ollivier@druil.net>
2012-03-30 07:29:34 +0200

    Added some comments to structure logic

--- src/net/druil/Canv4a/CanvasThread.java
+++ src/net/druil/Canv4a/CanvasThread.java
@@ -22,12 +22,22 @@ public class CanvasThread {
 		posts.add(p_orig);
 	}
 	
+	
+	/**
+	 * Gets the entire thread from one CanvasPost
+	 */
 	public void getEntireThread(){
 		// This will complete the tread from the first given element
 		if(posts.size()>=1){
 			String op_id = posts.get(0).thread_op_id;
 			if(posts.get(0).id == op_id) { //if the first element is really the OP
 				//then get all in response
+				for(int i=0; i<posts.size(); i++) { // we go through all the posts
+					for(int j=0; j<posts.get(i).replies.size(); j++) { // then go through all the replies
+						// and we get the reply, which is a new CanvasPost Object,
+						// Construct it, and add it to this.posts
+					}
+				}
 			}
 			else {
 				// then we must request the OP
